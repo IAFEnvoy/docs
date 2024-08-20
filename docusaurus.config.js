0d46048063e243ca0d333603a22c7bc16cb14b22
IAFEnvoy <tanksheng545@126.com>
2024-08-20 23:11:47 +0800

    add info

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -113,10 +113,14 @@ const config = {
           {
             title: 'Docs',
             items: [
-              // {
-              //   label: 'Tutorial',
-              //   to: '/docs/intro',
-              // },
+              {
+                label: 'FAQ',
+                to: '/docs/faq',
+              },
+              {
+                label: 'Mod Informations',
+                to: '/docs/info',
+              },
             ],
           },
           {
